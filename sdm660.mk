e4e02309e068b884beb354764df3aa346f5efd55
Jhonse02 <jhonse021220@gmail.com>
2020-05-23 17:33:57 -0500

    sdm660: Build libbtconfigstore from source
    Necessary to make FM Radio works.

--- sdm660.mk
+++ sdm660.mk
@@ -106,7 +106,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libsndmonitor \
     libhdmiedid \
-    libhfp
+    libhfp \
+    libbtconfigstore
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
